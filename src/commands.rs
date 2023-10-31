ce8dace9f734a14acbc5f52c5dddc17ecd7abd96
Owusu Kelvin Clark <kelvinclark2k77@gmail.com>
2023-10-31 10:35:56 +0100

    Implement the encode function

--- src/commands.rs
+++ src/commands.rs
@@ -1,5 +1,9 @@
 use clap::{Parser, Subcommand};
+use std::fs;
+use std::io::{Read, Write};
 use std::path;
+
+use crate::chunk_type::ChunkType;
 
 #[derive(Parser)]
 pub struct Cli {
@@ -31,4 +35,40 @@ enum Command {
         #[arg(short, long)]
         chunk_type: String,
     },
+}
+
+impl Cli {
+    pub fn run() -> crate::Result<()> {
+        let cli = Cli::parse();
+
+        match cli.command {
+            Command::Encode {
+                file_path,
+                chunk_type,
+                message,
+                output_file,
+            } => todo!(),
+            Command::Decode {
+                file_path,
+                chunk_type,
+            } => todo!(),
+            Command::Remove {
+                file_path,
+                chunk_type,
+            } => todo!(),
+        }
+        todo!()
+    }
+
+    fn encode(png_file: path::PathBuf, chunk_type: ChunkType, message: &str) -> crate::Result<()> {
+        let mut options = fs::OpenOptions::new();
+        let mut png_file = options.read(true).write(true).open(png_file)?;
+        let mut png_bytes: Vec<u8> = Vec::new();
+        png_file.read_to_end(&mut png_bytes)?;
+        let mut png = crate::png::Png::try_from(png_bytes.as_slice())?;
+        let chunk =
+            crate::chunk::Chunk::new(chunk_type, message.as_bytes().iter().copied().collect());
+        png.append_chunk(chunk);
+        Ok(png_file.write_all(&png.as_bytes())?)
+    }
 }
